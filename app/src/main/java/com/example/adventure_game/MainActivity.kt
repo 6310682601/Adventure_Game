970418b2b593e6ee5d97dea134ee60efe70ec74a
munin <6310682601@student.tu.ac.th>
2023-04-01 16:02:35 +0700

    create page

--- app/src/main/java/com/example/adventure_game/MainActivity.kt
+++ app/src/main/java/com/example/adventure_game/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.adventure_game
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.adventure_game.ui.theme.Adventure_GameTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Adventure_Game()
                 }
             }
         }
@@ -30,17 +42,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Adventure_Game(modifier: Modifier = Modifier) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFF4A148C))
+        ) {
+            Text(
+                text = "Adventure Game",
+                fontSize = 20.sp,
+                color = Color.White,
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(
+            text = "Story",
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(400.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+            ) {
+            Text(
+                text = "Choice 1",
+                fontSize = 20.sp
+            )
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Choice 2",
+                fontSize = 20.sp)
+        }
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Adventure_GameTheme {
-        Greeting("Android")
+        Adventure_Game()
     }
 }
